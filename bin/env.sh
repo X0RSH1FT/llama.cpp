3664e86f0fd73a5752d852c86665229a2b19f708
X0RSH1FT <X0RSH1FT@users.noreply.github.com>
2023-12-30 16:07:49 -0500

    Updated bash scripts. Added execute permissions. Modified default bin path.

--- bin/env.sh
+++ bin/env.sh
@@ -19,7 +19,7 @@ echo "Loading environment - $0" >&2
 
 # Defaults
 DEFAULT_LLAMA_CPP_PROJECT_PATH="$HOME/llama-cpp"
-DEFAULT_LLAMA_CPP_BIN_PATH="${DEFAULT_LLAMA_CPP_PROJECT_PATH}/build/bin/main.exe"
+DEFAULT_LLAMA_CPP_BIN_PATH="${DEFAULT_LLAMA_CPP_PROJECT_PATH}/build/bin/main"
 DEFAULT_LLM_MODEL_PATH="/data/llm/dolphin-2_6-phi-2-GGUF/dolphin-2_6-phi-2.Q4_K_M.gguf"
 
 # Apply configuration
